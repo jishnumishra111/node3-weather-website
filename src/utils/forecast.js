c4a1f9a5d2104bdc22d2d6c600fd0b90a57952b6
jishnumishra111 <jishnumishra111@gmail.com>
2023-05-07 20:35:27 +0530

    corrected the punctuation

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log(body)
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' .It is currently ' 
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' 
             + body.current.temperature+ ' degress out. ' 
             + 'It feels like '+body.current.feelslike
             + '. There is a ' + body.current.precip + '% chance of rain.'
